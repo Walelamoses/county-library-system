aaac935cd77d68fdba1718f71a6d6f6d1c5133b7
Walelamoses <87860032+Walelamoses@users.noreply.github.com>
2021-07-23 15:17:55 +0300

    i have created add user menu

---
+++ main library management system.c
@@ -0,0 +1,28 @@
+/*
+Project:Library Management system
+author :Walela Moses
+Date:June
+Compiler:c99
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("/tcountry management library system!/n");
+          printf("welcome Moses!\n");
+
+   //navigation menu
+
+   printf("1.add.user!/n");
+   printf("1.add.user!/n");
+   printf("2.add.book!/n");
+   printf("3.add.exit!/n");
+   printf("select action(1-3):");
+//v
+}alidate input
+      scanf("%d",&action);
+if (action<1|| action>3)
+      printf("invalid action.try again/n")
+    return 0;
